2016a5ca9e1a7bb48e98cb7e6290c1216275d49d
Tejas-Sanveo <tpatil@sanveo.com>
2024-12-26 19:16:15 +0530

    added api to wake up serve

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Res } from '@nestjs/common';
+import { Response } from 'express';
 
-@Controller()
+@Controller({ path: '', version: '1' })
 export class AppController {
   constructor() {}
+
+  @Get('serverStatus')
+  async serverStatus(@Res() response: Response) {
+    try {
+      return response.status(HttpStatus.OK).send({ message: 'Server is awake and running' });
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('Internal server error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
