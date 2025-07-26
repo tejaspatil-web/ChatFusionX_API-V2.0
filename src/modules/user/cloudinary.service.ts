d9581f6f0861a9aacfce8b8537aefe85b4a4ca1f
Tejas Patil <tejasp094@gmail.com>
2025-07-26 21:11:29 +0530

    code added for upload profile photo and update profile

---
+++ src/modules/user/cloudinary.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import {
+  v2 as cloudinary,
+  UploadApiResponse,
+  UploadApiErrorResponse,
+} from 'cloudinary';
+import toStream = require('buffer-to-stream');
+
+@Injectable()
+export class CloudinaryService {
+  async uploadUserProfile(
+    file: any,
+  ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    return new Promise((resolve, reject) => {
+      const upload = cloudinary.uploader.upload_stream(
+        { folder: 'chatfusionx-user-profiles' },
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        },
+      );
+      toStream(file.buffer).pipe(upload);
+    });
+  }
+}
